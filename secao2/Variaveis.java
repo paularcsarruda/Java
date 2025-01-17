ead7bb639e0241ee412c9cb9283853edc53ed16e
Paula Arruda <122739036+paularcsarruda@users.noreply.github.com>
2025-01-17 09:57:32 -0300

    Add files via upload

---
+++ secao2/Variaveis.java
@@ -0,0 +1,14 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int age = 37;
+        String name = "Paula Arruda";
+        double height = 1.58;
+        boolean pet = true;
+
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Pet: " + pet);
+    }
+}
